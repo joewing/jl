b3a40c0274f4e5417200fc8e618796194e459746
Joe Wingbermuehle <joewing@joewing.net>
2013-08-15 21:45:17 -0500

    Add file support.

--- repl.c
+++ repl.c
@@ -15,7 +15,6 @@ JLValue *PrintFunc(struct JLContext *context, JLValue *arglist)
 {
    JLValue *vp = arglist->next;
    JLValue *result = arglist;
-   printf("\n");
    for(; vp; vp = vp->next) {
       result = JLEvaluate(context, vp);
       JLPrint(context, result);
@@ -24,20 +23,42 @@ JLValue *PrintFunc(struct JLContext *context, JLValue *arglist)
    return result;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
    char *line = NULL;
    const char *to_parse;
    struct JLContext *context;
    JLValue *value;
+   JLValue *result;
    size_t cap = 0;
+   char *filename = NULL;
+   FILE *fd;
+
+   if(argc == 2) {
+      filename = argv[1];
+   } else if(argc != 1) {
+      printf("usage: %s <file>\n", argv[0]);
+      return -1;
+   }
 
    context = JLCreateContext();
    JLDefineSpecial(context, "print", PrintFunc);
 
-   for(;;) {
-      printf("> "); fflush(stdout);
-      ssize_t len = getline(&line, &cap, stdin);
+   if(filename) {
+      fd = fopen(filename, "r");
+      if(!fd) {
+         printf("ERROR: file \"%s\" not found\n", filename);
+         return -1;
+      }
+   } else {
+      fd = stdin;
+   }
+
+   while(!feof(fd)) {
+      if(!filename) {
+         printf("> "); fflush(stdout);
+      }
+      ssize_t len = getline(&line, &cap, fd);
       if(len <= 0) {
          break;
       }
@@ -47,13 +68,19 @@ int main()
          if(value == NULL) {
             break;
          }
-         printf("=> ");
-         JLPrint(context, JLEvaluate(context, value));
-         printf("\n");
+         result = JLEvaluate(context, value);
+         if(!filename) {
+            printf("=> ");
+            JLPrint(context, result);
+            printf("\n");
+         }
       }
    }
    if(line) {
       free(line);
+   }
+   if(filename) {
+      fclose(fd);
    }
    JLDestroyContext(context);
 
