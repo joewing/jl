9c29dce735cb4f8fd2079a38f9510bf711480b72
Joe Wingbermuehle <joewing@joewing.net>
2013-08-27 12:50:02 -0500

    Build a shared object.

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
 
-CC=gcc
-CFLAGS=-O2 -Wall -Werror -I. -g
-LDFLAGS=-g
+CC=clang
+CFLAGS=-O2 -Wall -Werror -I. -fpic -fvisibility=hidden
+LDFLAGS=
 
 JLOBJS=src/jl.o src/jl-context.o src/jl-func.o src/jl-scope.o src/jl-value.o
 REPLOBJS=src/jli.o jl.a
 
-all: jli jl.a
+all: jli jl.a libjl.so
 
 jli: $(REPLOBJS)
 	$(CC) $(LDFLAGS) $(REPLOBJS) -o jli
+	strip jli
+
+libjl.so: $(JLOBJS)
+	$(CC) $(LDFLAGS) -shared $(JLOBJS) -Wl,--version-script=jl.map -o libjl.so
+	strip libjl.so
 
 jl.a: $(JLOBJS)
 	ar r jl.a $(JLOBJS)
@@ -18,5 +23,5 @@ jl.a: $(JLOBJS)
 	$(CC) $(CFLAGS) -c $*.c -o $*.o
 
 clean:
-	rm -f jli jl.a src/*.o
+	rm -f jli jl.a libjl.so src/*.o
 
--- jl.h
+++ jl.h
@@ -12,6 +12,12 @@
 #define JL_VERSION_MAJOR   0
 #define JL_VERSION_MINOR   1
 
+#if defined _WIN32 || defined __CYGWIN__
+#  define JLEXPORT __declspec(dllexport)
+#else
+#  define JLEXPORT __attribute__((visibility("default")))
+#endif
+
 struct JLValue;
 struct JLContext;
 
@@ -27,17 +33,20 @@ typedef struct JLValue *(*JLFunction)(struct JLContext *context,
 /** Create a context for running JL programs.
  * @return The context.
  */
+JLEXPORT
 struct JLContext *JLCreateContext();
 
 /** Destroy a JL context.
  * @param context The context to be destroyed.
  */
+JLEXPORT
 void JLDestroyContext(struct JLContext *context);
 
 /** Increase the reference count of a value.
  * @param context The context containing the value.
  * @param value The value (can be NULL).
  */
+JLEXPORT
 void JLRetain(struct JLContext *context, struct JLValue *value);
 
 /** Decrease the reference count of a value.
@@ -45,6 +54,7 @@ void JLRetain(struct JLContext *context, struct JLValue *value);
  * @param context The context.
  * @param value The value to be released (can be NULL).
  */
+JLEXPORT
 void JLRelease(struct JLContext *context, struct JLValue *value);
 
 /** Define a value.
@@ -53,6 +63,7 @@ void JLRelease(struct JLContext *context, struct JLValue *value);
  * @param name The name of the binding.
  * @param value The value to insert.
  */
+JLEXPORT
 void JLDefineValue(struct JLContext *context,
                    const char *name,
                    struct JLValue *value);
@@ -65,6 +76,7 @@ void JLDefineValue(struct JLContext *context,
  * @param name The name of the function.
  * @param func The function code.
  */
+JLEXPORT
 void JLDefineSpecial(struct JLContext *context,
                      const char *name,
                      JLFunction func);
@@ -76,6 +88,7 @@ void JLDefineSpecial(struct JLContext *context,
  * @param value The value to define.
  * @return The value.  This value must be released if not used.
  */
+JLEXPORT
 struct JLValue *JLDefineNumber(struct JLContext *context,
                                const char *name,
                                double value);
@@ -86,6 +99,7 @@ struct JLValue *JLDefineNumber(struct JLContext *context,
  * @param line The expression to be parsed.
  * @return The expression.  This value must be released if not used.
  */
+JLEXPORT
 struct JLValue *JLParse(struct JLContext *context, const char **line);
 
 /** Evaluate an expression.
@@ -93,54 +107,63 @@ struct JLValue *JLParse(struct JLContext *context, const char **line);
  * @param value The expression to evaluate.
  * @return The result.  This value must be released if not used.
  */
+JLEXPORT
 struct JLValue *JLEvaluate(struct JLContext *context, struct JLValue *value);
 
 /** Determine if a value is a number.
  * @param value The value to check (NULL is allowed).
  * @return 1 if a number, 0 otherwise.
  */
+JLEXPORT
 char JLIsNumber(struct JLValue *value);
 
 /** Get the number representation of a value.
  * @param value The value (must be a non-NULL number value).
  * @return The numeric value.
  */
+JLEXPORT
 double JLGetNumber(struct JLValue *value);
 
 /** Determine if a value is a string.
  * @param value The value to check (NULL is allowed).
  * @return 1 if a string, 0 otherwise.
  */
+JLEXPORT
 char JLIsString(struct JLValue *value);
 
 /** Get the string representation of a value.
  * @param value The value (must be a non-NULL string value).
  * @return A NULL-terminated string.
  */
+JLEXPORT
 const char *JLGetString(struct JLValue *value);
 
 /** Determine if a value is a list.
  * @param value The value to check.
  * @return 1 if a list, 0 otherwise.
  */
+JLEXPORT
 char JLIsList(struct JLValue *value);
 
 /** Get the first item of a list.
  * @param value The list (must be a non-NULL list value).
  * @return The first item in the list (possibly NULL).
  */
+JLEXPORT
 struct JLValue *JLGetHead(struct JLValue *value);
 
 /** Get the next item in a list.
  * @param value The previous value in the list (must be non-NULL).
  * @return The next item in the list (possibly NULL).
  */
+JLEXPORT
 struct JLValue *JLGetNext(struct JLValue *value);
 
 /** Display a value.
  * @param context The context.
  * @param value The value to display.
  */
+JLEXPORT
 void JLPrint(const struct JLContext *context, const struct JLValue *value);
 
 #endif /* JL_H */
